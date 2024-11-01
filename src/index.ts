b776d97901f6429fecd62561bfda44653f7ca594
Elmay05 <fewdinosaur@gmail.com>
2024-10-31 22:28:51 -0600

    LISENCE

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,30 @@
-import { config } from "dotenv";
+import dotenvFlow from "dotenv-flow";
 import express from "express";
 import studentRouter from "./routes/student";
+import testRoutes from "./routes/test";
+import unknownResource from "./middlewares/unknown-resource";
+import unknownError from "./middlewares/unknown-error";
+import validationError from "./middlewares/validation-error";
 //Para poder acceder a las variables del ambiente (.env)
-config();
+//if(process.env.NODE_ENV!=="production"){
+dotenvFlow.config();
 
 const app = express();
 
 // Middleware para parsear JSON
 app.use(express.json());
+// Rutas de api
+app.use("/api/v1/student", studentRouter);
 
-app.use("/student", studentRouter);
+//Rutas de prueva
+app.use("/error", testRoutes);
+
+// Middlewares
+app.use(validationError);
+app.use(unknownResource); //Error 404
+
+//Middleware de error
+app.use(unknownError);
 
 app.listen(process.env.SERVER_PORT, function () {
   console.log("Escuchando puerto " + process.env.SERVER_PORT);
--- src/routes/student.ts
+++ src/routes/student.ts
@@ -1,16 +1,26 @@
 // routes/usuarios.ts
 import { Router } from "express";
 import {
+  createStudent,
   deleteStudent,
   getStudents,
   updateStudent,
 } from "../controllers/student";
+import validate from "../middlewares/validate";
+import { studentSchema } from "../schemas/student";
 
 const router = Router();
 
 // Regresa todos los alumnos en la base de datos
-
 router.get("/", getStudents);
+
+// Crea un alumno nuevo en la BD
+router.post("/", validate(studentSchema), createStudent);
+
+// Actualiza los datos de un alumno en la BD
+router.put("/:id", validate(studentSchema), updateStudent);
+
+// Borra los datos de un alumno en la BD
 router.delete("/:id", deleteStudent);
-router.put("/:id", updateStudent);
+
 export default router;
